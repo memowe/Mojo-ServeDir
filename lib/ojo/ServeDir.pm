948e22cf3464f9ad814a4b8e7c895505effc37a9
Mirko Westermeier <mirko@westermeier.de>
2019-11-07 11:26:16 +0100

    Add Module::Starter boilerplate

---
+++ lib/ojo/ServeDir.pm
@@ -0,0 +1,109 @@
+package ojo::ServeDir;
+
+use 5.020;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+ojo::ServeDir - The great new ojo::ServeDir!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use ojo::ServeDir;
+
+    my $foo = ojo::ServeDir->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mirko Westermeier, C<< <mirko at westermeier.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ojo-servedir at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=ojo-ServeDir>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc ojo::ServeDir
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=ojo-ServeDir>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/ojo-ServeDir>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/ojo-ServeDir>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/ojo-ServeDir>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by Mirko Westermeier.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+
+=cut
+
+1; # End of ojo::ServeDir
